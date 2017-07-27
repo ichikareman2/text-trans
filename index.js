099ae317f0ea679d64371c6bc4fc0fffcca7b4d0
willard <willardquing@gmail.com>
2017-07-27 08:08:57 +0800

    with html

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const electron = require('electron');
 const BrowserWindow = electron.BrowserWindow;
+const url = require("url");
+const path = require("path");
 const app = electron.app;
 
-app.on("ready", createWindow);
 
+let win;
 function createWindow(){
-    let win = new BrowserWindow({width:490, height:225})
+    win = new BrowserWindow({width:490, height:225})
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file:",
+        slashes: true
+    }))
 }
+
+app.on("ready", createWindow);
+app.on("closed", () => {
+    win = null;
+})
