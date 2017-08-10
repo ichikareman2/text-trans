6a033364c40937497d8095b1dfd4658ed8b393c5
willard <willardquing@gmail.com>
2017-08-10 19:41:17 +0800

    - use ascii superscript instead of html superscript

--- api/text-transformers/transforms/superscript-transformer.js
+++ api/text-transformers/transforms/superscript-transformer.js
@@ -1,8 +1,41 @@
-let transformer = require("./transformer").transformer;
+const transformer = require("./transformer").transformer;
+class pair {
+    constructor(char, trans) {
+        this.char = char;
+        this.trans = trans;
+    }
+}
+let numbers = "0123456789";
+let alphabet = "abcdefghijklmnoprstuvwxz"
+
+let numberTrans = "⁰¹²³⁴⁵⁶⁷⁸⁹"
+let alphabetTrans = "ᵃᵇᶜᵈᵉᶠᵍʰⁱʲᵏˡᵐⁿᵒᵖʳˢᵗᵘᵛʷˣʸᶻ"
+
+let orig = numbers.concat(alphabet);
+let transformed = numberTrans.concat(alphabetTrans);
+
+let pairs = Array.prototype.map.call(orig, (x, i) => new pair(x, transformed[i]));
 
 class superscriptTransformer extends transformer {
-    transform (text) {
-        return text.sup();
+    constructor() {
+        super();
+        this.pairs = pairs;
+    }
+    transform(text) {
+        // return text.sup();
+        return this._transform(text);
+    }
+
+    _transform(text) {
+        // let toBeReplaced = Array.prototype.filter.call(text, (x, i) => text.indexOf(x) === i)
+        let result = text;
+        Array.prototype.forEach.call(result, (c) => {
+            if (result.indexOf(c) > -1 && this.pairs.findIndex(x => x.char === c) > -1)
+                result = result.replace(c, this.pairs.find(x => x.char === c).trans)
+        })
+        return result;
+        // toBeReplaced.forEach(x => result = result.replace(x, this.pairs.find(y => y.char === x).trans))
+        // return result;
     }
 }
 
